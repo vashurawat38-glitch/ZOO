dd91787f46c86f3cc79eaed2b5a3396eee103c4b
vashu rawat <vashurawat38@gmail.com>
2026-07-13 01:11:19 +0530

    update mongo uri

--- backend/backend/Server.js
+++ backend/backend/Server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.get('/',(req,res)=>{res.send('API is running');});
 //  MongoDB Atlas connection
-mongoose.connect("mongodb+srv://vashurawat24_db_user:rawat100@cluster0.nmm9r9b.mongodb.net/?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Atlas se connect ho gaya!"))
   .catch(err => console.error("❌ Connection Error:", err));
 
