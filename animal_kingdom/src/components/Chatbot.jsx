2e9a77c2a3bd23aa41bb0c7c5989156a89a2124b
vashu rawat <vashurawat38@gmail.com>
2026-09-26 00:18:37 +0530

    fixed chatbot

--- animal_kingdom/src/components/Chatbot.jsx
+++ animal_kingdom/src/components/Chatbot.jsx
@@ -14,7 +14,7 @@ export default function Chatbot() {
     setMessages(newMessages);
 
     try {
-      const res = await fetch("http://localhost:5000/api/chatbot", {
+      const res = await fetch("https://zoo-qnls.onrender.com/api/chatbot", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input })
